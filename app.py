1af4db4ac70df2d5deca5e74c263dd4b3996afc8
mlkwon <lk38ll@gmail.com>
2024-07-29 10:31:40 +0000

    initial commit (240729)

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ for idx, row in travel_data.iterrows():
     popup_text = f"""
     <div style='width: 300px;'>
     <b>{row['Location']}</b><br>
-    English: {row['Contents']} ({row['Type']})<br>
+    Type: {row['Type']} <br>
+    English: {row['Contents']}<br>
     Korean: {row['Contents (Korean)']}<br>
     <a href="{row['URL']}" target="_blank">More info</a>
     """
